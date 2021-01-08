4eae255e8ddcaa13b6975b3531cd59bca674fe69
fuyafeiui <1090352182@qq.com>
2021-01-08 18:57:35 +0800

    项目开发更新

---
+++ src/views/home/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar.vue";
+import {getHomeMultidata} from 'network/home.js'; 
+export default {
+  name: 'Home',
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      result: null
+    }
+  },
+  created() {
+    //1.请求多个数据
+    getHomeMultidata().then(res =>{
+      this.result = res;
+    })
+  }
+}
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  publicPath: '/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network'
+      }
+    }
+  }
+}
