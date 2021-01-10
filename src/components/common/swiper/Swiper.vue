451a79c610d8ea7856b8ba61ead8d02f3c2d2598
fuyafeiui <1090352182@qq.com>
2021-01-10 12:07:19 +0800

    更新数据

--- src/components/common/swiper/Swiper.vue
+++ src/components/common/swiper/Swiper.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="hy-swiper">
-    <div class="swiper" @touchstart="touchstart" @touchmove="touchmove" @touchend="touchend">
+    <div class="swiper" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd">
       <slot></slot>
     </div>
     <div class="indicator">
@@ -210,5 +210,36 @@ export default {
 </script>
 
 <style scoped>
+  #hy-swiper {
+    overflow: hidden;
+    position: relative;
+  }
 
+  .swiper {
+    display: flex;
+  }
+
+  .indicator {
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    width: 100%;
+    bottom: 8px;
+  }
+
+  .indi-item {
+    box-sizing: border-box;
+    width: 8px;
+    height: 8px;
+    border-radius: 4px;
+    background-color: #fff;
+    line-height: 8px;
+    text-align: center;
+    font-size: 12px;
+    margin: 0 5px;
+  }
+
+  .indi-item.active {
+    background-color: rgba(212,62,46,1.0);
+  }
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,26 +1,33 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners" />
+
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar.vue";
-import {getHomeMultidata} from 'network/home.js'; 
+import HomeSwiper from './childComps/HomeSwiper';
+import {getHomeMultidata} from 'network/home.js';
+
 export default {
   name: 'Home',
   components: {
-    NavBar
+    NavBar,
+    HomeSwiper
   },
   data() {
     return {
-      result: null
+      result: null,
+      banners: []
     }
   },
   created() {
     //1.请求多个数据
     getHomeMultidata().then(res =>{
       this.result = res;
+      this.banners = res.data.banner.list;
     })
   }
 }
