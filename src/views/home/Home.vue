8672daed80f42a307fd0da0f773b536c8e7f8c52
fuyafeiui <1090352182@qq.com>
2021-02-06 16:05:24 +0800

    更新

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,45 +1,146 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners" />
-    <recommend-view :recommends="recommends" />
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <scroll>
+      <home-swiper :banners="banners" />
+      <recommend-view :recommends="recommends" />
+      <feature-view />
+      <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick" />
+      <goods-list :goods="showGoods" />
+    </scroll>
+
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar.vue";
-import HomeSwiper from './childComps/HomeSwiper';
-import RecommendView from './childComps/RecommendView';
-import {getHomeMultidata} from 'network/home.js';
+  //1.导入的home组件
+  import HomeSwiper from './childComps/HomeSwiper';
+  import RecommendView from './childComps/RecommendView';
+  import FeatureView from './childComps/FeatureView';
 
-export default {
-  name: 'Home',
-  components: {
-    NavBar,
-    HomeSwiper,
-    RecommendView
-  },
-  data() {
-    return {
-      result: null,
-      banners: [],
-      recommends: []
+  //2.导入的项目公共组件
+  import NavBar from "components/common/navbar/NavBar.vue";
+  import TabControl from 'components/content/tabControl/TabControl';
+  import GoodsList from 'components/content/goods/GoodsList';
+  import Scroll from 'components/common/scroll/Scroll';
+  //3.导入的方法
+  import { getHomeMultidata, getHomeGoods } from 'network/home.js';
+
+  export default {
+    name: 'Home',
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      TabControl,
+      GoodsList,
+      Scroll
+    },
+    data() {
+      return {
+        result: null,
+        banners: [],
+        recommends: [],
+        goods: {
+          'pop': { page: 0, list: [] },
+          'new': { page: 0, list: [] },
+          'sell': { page: 0, list: [] }
+        },
+        currentType: 'pop'
+      }
+    },
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list;
+      }
+    },
+    created() {
+      //1.请求多个数据
+      this.getHomeMultidata()
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods: {
+      /**
+       * 事件监听的方法
+       * 
+      **/
+      tabClick(index) {
+        switch (index) {
+          case 0:
+            this.currentType = 'pop'
+            break
+          case 1:
+            this.currentType = 'new'
+            break
+          case 2:
+            this.currentType = 'sell'
+            break
+        }
+      },
+
+      /*
+      网络请求相关的方法
+      **/
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.result = res;
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1;
+        getHomeGoods(type, page).then(res => {
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+        });
+      }
     }
-  },
-  created() {
-    //1.请求多个数据
-    getHomeMultidata().then(res =>{
-      this.result = res;
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    })
   }
-}
 </script>
 
 <style scoped>
+  #home {
+    /* padding-top: 44px; */
+    height: 100vh;
+    position: relative;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
   }
+
+  /* 设置tabControl的滚动粘滞 */
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+  }
+
+  .content {
+    overflow: hidden;
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
+
+  /* 一种方法 */
+  /* .content {
+    height: calc(100% - 93px);
+    overflow: hidden;
+    margin-top: 44px;
+  } */
 </style>
